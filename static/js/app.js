bdf0fc4c3230b37e9573fe26582725b5ce01907d
Shockman1000 <60047928+Shockman1000@users.noreply.github.com>
2020-07-26 13:14:50 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -17,27 +17,26 @@ function Build_table_data(data,colums){
     });
 };
 
-function handleClick(){
-filterTable();
-}
-debugger.selectAll("#filter-btn").on("click", handleClick);
-function filterTable() {//Filter table
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("datetime");// datetime
-    filter = input.value.toUpperCase();
-    table = document.getElementById("pageData");//DF
-    tr = table.getElementsByTagName("tr");//tr
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
-  }
 
+function handleClick() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("datetime");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("pageData");
+  console.log(table)
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
 Build_table_data(tableData);
